8153dc36a9f464a7f72b2609034cc92aabccfd22
sergey s <som-ana@yandex.ru>
2023-06-24 19:38:57 +0300

    added test no error

--- src/test/java/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/java/ru/netology/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("data some"));
+                .body("data", equalTo("some data"));
     }
 }
