2e1d684fea4dac130a8eb6ed70e5c4477f813e5f
sergey s <som-ana@yandex.ru>
2023-06-24 19:30:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/4i1r3oidavsg4rnp?svg=true)](https://ci.appveyor.com/project/Sergei37964/autotestingdz2-3)
---
+++ src/test/java/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package java.ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("data some"));
+    }
+}
